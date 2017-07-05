42977b607aaf36182af51bd408755a331f51b17c
Vladislav Struzik <struzik.vladislav@gmail.com>
2017-07-05 04:31:33 +0300

    Host information command.

---
+++ src/Request/Host/Info.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Struzik\EPPClient\Request\Host;
+
+use Struzik\EPPClient\Request\AbstractRequest;
+use Struzik\EPPClient\Response\Host\Info as InfoResponse;
+use Struzik\EPPClient\Node\Common\Info as InfoNode;
+use Struzik\EPPClient\Node\Common\Command;
+use Struzik\EPPClient\Node\Common\TransactionId;
+use Struzik\EPPClient\Node\Host\Info as HostInfoNode;
+use Struzik\EPPClient\Node\Host\Name;
+
+/**
+ * Object representation of the request of host information command.
+ */
+class Info extends AbstractRequest
+{
+    /**
+     * @var string
+     */
+    private $host;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function handleParameters()
+    {
+        $epp = $this->getRoot();
+
+        $command = new Command($this);
+        $epp->append($command);
+
+        $info = new InfoNode($this);
+        $command->append($info);
+
+        $hostInfo = new HostInfoNode($this);
+        $info->append($hostInfo);
+
+        $hostName = new Name($this, ['host' => $this->host]);
+        $hostInfo->append($hostName);
+
+        $transaction = new TransactionId($this);
+        $command->append($transaction);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getResponseClass()
+    {
+        return InfoResponse::class;
+    }
+
+    /**
+     * Getting the name of the host.
+     *
+     * @return string
+     */
+    public function getHost()
+    {
+        return $this->host;
+    }
+
+    /**
+     * Setting the name of the host.
+     *
+     * @param string $host fully qualified name of the host object
+     *
+     * @return self
+     */
+    public function setHost($host)
+    {
+        $this->host = $host;
+
+        return $this;
+    }
+}
---
+++ src/Response/Host/Info.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace Struzik\EPPClient\Response\Host;
+
+use Struzik\EPPClient\Response\AbstractCommonResponse;
+
+/**
+ * Object representation of the response of host information command.
+ */
+class Info extends AbstractCommonResponse
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function isSuccess()
+    {
+        return $this->getResultCode() === self::RC_SUCCESS;
+    }
+
+    /**
+     * Fully qualified name of the host object.
+     *
+     * @return string
+     */
+    public function getHost()
+    {
+        $node = $this->getFirst('//epp:epp/epp:response/epp:resData/host:infData/host:name');
+
+        return $node->nodeValue;
+    }
+
+    /**
+     * Unique identifier in the registry.
+     *
+     * @return string
+     */
+    public function getRepositoryId()
+    {
+        $node = $this->getFirst('//epp:epp/epp:response/epp:resData/host:infData/host:roid');
+
+        return $node->nodeValue;
+    }
+
+    /**
+     * List of object statuses.
+     *
+     * @return \Generator
+     */
+    public function getStatusList()
+    {
+        $nodes = $this->get('//epp:epp/epp:response/epp:resData/host:infData/host:status');
+
+        foreach ($nodes as $node) {
+            yield $node->getAttribute('s');
+        }
+    }
+
+    /**
+     * Check status existence.
+     *
+     * @param string $status string representation of the status
+     *
+     * @return bool
+     */
+    public function statusExist($status)
+    {
+        $generator = $this->getStatusList();
+        $list = iterator_to_array($generator);
+
+        return in_array($status, $list);
+    }
+
+    /**
+     * Getting a list of assigned IP addresses.
+     *
+     * @return \Generator
+     */
+    public function getIPList()
+    {
+        $nodes = $this->get('//epp:epp/epp:response/epp:resData/host:infData/host:addr');
+
+        foreach ($nodes as $node) {
+            yield (object) ['address' => $node->nodeValue, 'type' => $node->getAttribute('ip')];
+        }
+    }
+
+    /**
+     * The identifier of the registrar.
+     *
+     * @return string
+     */
+    public function getClientId()
+    {
+        $node = $this->getFirst('//epp:epp/epp:response/epp:resData/host:infData/host:clID');
+
+        return $node->nodeValue;
+    }
+
+    /**
+     * The identifier of the creator (registry).
+     *
+     * @return string
+     */
+    public function getCreatorId()
+    {
+        $node = $this->getFirst('//epp:epp/epp:response/epp:resData/host:infData/host:crID');
+
+        return $node->nodeValue;
+    }
+
+    /**
+     * Date and time of host object creation.
+     *
+     * @param string $format format of the date string
+     *
+     * @return \DateTime | null
+     */
+    public function getCreateDate($format)
+    {
+        $node = $this->getFirst('//epp:epp/epp:response/epp:resData/host:infData/host:crDate');
+        if ($node) {
+            return \DateTime::createFromFormat($format, $node->nodeValue);
+        }
+
+        return;
+    }
+
+    /**
+     * The identifier of the updater (registry).
+     *
+     * @return string | null
+     */
+    public function getUpdaterId()
+    {
+        $node = $this->getFirst('//epp:epp/epp:response/epp:resData/host:infData/host:upID');
+        if (is_null($node)) {
+            return;
+        }
+
+        return $node->nodeValue;
+    }
+
+    /**
+     * Date and time of host object updating.
+     *
+     * @param string $format format of the date string
+     *
+     * @return \DateTime | null
+     */
+    public function getUpdateDate($format)
+    {
+        $node = $this->getFirst('//epp:epp/epp:response/epp:resData/host:infData/host:upDate');
+        if ($node) {
+            return \DateTime::createFromFormat($format, $node->nodeValue);
+        }
+
+        return;
+    }
+
+    /**
+     * Date and time of host object transferring.
+     *
+     * @param string $format format of the date string
+     *
+     * @return \DateTime | null
+     */
+    public function getTransferDate($format)
+    {
+        $node = $this->getFirst('//epp:epp/epp:response/epp:resData/host:infData/host:trDate');
+        if ($node) {
+            return \DateTime::createFromFormat($format, $node->nodeValue);
+        }
+
+        return;
+    }
+}
